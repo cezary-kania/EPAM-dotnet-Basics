bd7ddb2f760060784195f8fb03ec2cfd4af2257d
cezary-kania <cezary.kaniadev@gmail.com>
2023-05-16 20:27:26 +0200

    Add solution for module 3

---
+++ Module-3/FileSystemVisitorConsoleApp/FileSystemLib/FileSystemDirectory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSystemLib;
+
+public class FileSystemDirectory : IDirectory
+{
+    public IEnumerable<string> EnumerateFileSystemEntries(string path)
+    {
+        return Directory.EnumerateFileSystemEntries(path);
+    }
+
+    public bool Exists(string path)
+    {
+        return Directory.Exists(path);
+    }
+}
