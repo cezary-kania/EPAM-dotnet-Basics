dcb14051051dce5a5a8e40105024626c27dddba6
cezary-kania <cezary.kaniadev@gmail.com>
2023-06-17 09:24:03 +0200

    Implement BinaryGapCounter.Solution method
    Details:
    - Added implementation of Solution method

--- Module-7/BinaryGap/BinaryGapCounter.cs
+++ Module-7/BinaryGap/BinaryGapCounter.cs
@@ -4,6 +4,26 @@ public static class BinaryGapCounter
 {
     public static int Solution(int n)
     {
-        throw new NotImplementedException();
+        var binary = Convert.ToString(n, 2);
+
+        var maxGap = 0;
+        var currentGap = 0;
+        foreach (var digit in binary)
+        {
+            if (digit is '1')
+            {
+                if (currentGap > maxGap)
+                {
+                    maxGap = currentGap;
+                }
+
+                currentGap = 0;
+            }
+            else
+            {
+                currentGap++;
+            }
+        }
+        return maxGap;
     }
 }
