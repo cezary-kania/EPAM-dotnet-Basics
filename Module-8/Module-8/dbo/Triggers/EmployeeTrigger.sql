7d27f3bf08b5bd6c048ab0e0965970298753ee6b
cezary-kania <cezary.kaniadev@gmail.com>
2023-06-28 07:56:05 +0200

    Add solution for SQL Fundamentals home task

---
+++ Module-8/Module-8/dbo/Triggers/EmployeeTrigger.sql
@@ -0,0 +1,11 @@
+﻿CREATE TRIGGER [EmployeeTrigger]
+ON [dbo].[Employee]
+AFTER INSERT
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	INSERT INTO Company (Name, AddressId)
+	SELECT CompanyName, AddressId
+	FROM inserted;
+END
