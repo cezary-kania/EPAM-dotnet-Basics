950d4cd573df39a308265e2fa109e3615602ee13
cezary-kania <cezary.kaniadev@gmail.com>
2023-07-25 23:18:52 +0200

    Add Module-12 start files

---
+++ Module-12/Task1/DoNotChange/Supplier.cs
@@ -0,0 +1,9 @@
+﻿namespace Task1.DoNotChange;
+
+public class Supplier
+{
+	public string SupplierName { get; set; }
+	public string Address { get; set; }
+	public string City { get; set; }
+	public string Country { get; set; }
+}
